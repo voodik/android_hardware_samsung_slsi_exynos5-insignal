461658aabee203a5de68962836d18495451b98f4
voodik <voodik.am@gmail.com>
2014-07-23 01:32:32 +0400

    initial commit

---
+++ libcodec/video/v4l2/Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	dec/ExynosVideoDecoder.c \
+	enc/ExynosVideoEncoder.c
+
+LOCAL_C_INCLUDES := \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
+	$(LOCAL_PATH)/include \
+	$(TOP)/hardware/samsung_slsi/exynos/include
+
+LOCAL_ADDITIONAL_DEPENDENCIES += \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
+ifeq ($(BOARD_USE_KHRONOS_OMX_HEADER), true)
+LOCAL_C_INCLUDES += $(TOP)/hardware/samsung_slsi/openmax/include/khronos
+else
+LOCAL_C_INCLUDES += $(TOP)/frameworks/native/include/media/openmax
+endif
+
+LOCAL_MODULE := libExynosVideoApi
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRELINK_MODULE := false
+LOCAL_ARM_MODE := arm
+
+include $(BUILD_STATIC_LIBRARY)
